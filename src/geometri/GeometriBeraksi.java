dfd3706a9065b75d59c6207476072b30ca1cfa06
unknown <delvikarmilasari@gmail.com>
2023-06-14 02:30:54 +0800

    Membuat Class Main dan Menjalankan Program

---
+++ src/geometri/GeometriBeraksi.java
@@ -0,0 +1,20 @@
+
+package geometri;
+
+
+public class GeometriBeraksi {
+public static void main(String[] args) {
+   Lingkaran delvi= new Lingkaran(20);
+     System.out.println("Luas Lingkaran: "+delvi.luas());
+     System.out.println("Keliling Lingkaran: "+delvi.keliling());
+   PersegiPanjang delvi1= new PersegiPanjang (40, 60) ;
+     System.out.println("Luas Persegi Panjang: "+delvi1.luas());
+     System.out.println("Keliling Persegi Panjang: "+delvi1.keliling());
+    }
+    
+ }
+  
+    
+   
+           
+
